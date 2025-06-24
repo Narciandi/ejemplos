f9ddf22429f36a402f80bbb4c3464eed8718c2ff
Álvaro <alvaropereznarciandi@gmail.com>
2025-06-24 20:37:00 +0200

    Mejoro el gráfico anterior y añado gráfico de DOs por materia y CC

--- Distribucion competencias/10_AnalisisCompetencias.R
+++ Distribucion competencias/10_AnalisisCompetencias.R
@@ -1,5 +1,10 @@
 
 library(openxlsx)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(stringr)
+library(scales) # Para usar label_wrap
 
 rutaDatos <- "./datos/Descriptores ESO.xlsx"
 datos <- read.xlsx(xlsxFile = rutaDatos)
@@ -8,7 +13,7 @@ datos <- read.xlsx(xlsxFile = rutaDatos)
 # Análisis que puedo hacer:
 # Número total de descriptores por materia
 
-library(dplyr)
+
 
 datos %>% 
   group_by(Materia) %>% 
@@ -19,6 +24,32 @@ datos %>%
 # Número total de descriptores por CE
 # Análisis de descriptores por competencia clave
 # Peso de materia en cada CC
+
+#### DESCRIPTORES POR MATERIA ####
+
+datosGrafico <- datos %>% 
+  group_by(Materia) %>% 
+  summarise(nDOs = sum(CCL) + sum(CP) + sum(STEM) + sum(CD) + sum(CPSAA) + sum(CC) + sum(CE) + sum(CCEC)) %>%
+  arrange(desc(nDOs))
+
+datosGrafico$Materia <- factor(x = datosGrafico$Materia,
+                               levels = datosGrafico$Materia)
+
+grafico <- ggplot(datosGrafico) + 
+  geom_col(mapping = aes(x = Materia, y = nDOs)) + 
+  theme(axis.text.x = element_text(angle = 90),
+      panel.grid = element_line(colour = "black"),
+      panel.background = element_rect(fill = "white"),
+      legend.title = element_blank()) +
+  scale_x_discrete(labels = label_wrap(28)) +
+  ggtitle(label = "Número de descriptores operativos por materia",
+          subtitle = "ESO") +
+  xlab(label = "") +
+  ylab(label = "")
+
+grafico
+
+#### DESCRIPTORES POR MATERIA Y CC ####
 
 datosGrafico <- datos %>% 
   group_by(Materia) %>% 
@@ -33,17 +64,38 @@ datosGrafico <- datos %>%
             nCCEC = sum(CCEC)) %>%
   arrange(desc(nDOs))
 
-datosGrafico$Materia <- factor(x = datosGrafico$Materia,
-                               levels = datosGrafico$Materia)
+datosGrafico <- pivot_longer(data = datosGrafico,
+                             cols = c("nDOs", "nCCL", "nCP", "nSTEM", "nCD", "nCPSAA", "nCC", "nCE", "nCCEC"))
 
-library(ggplot2)
+filas <- datosGrafico$name != "nDOs"
+datosGrafico <- datosGrafico[filas,]
+datosGrafico$name <- str_replace(string = datosGrafico$name,
+            pattern = "n",
+            replacement = "") 
 
-grafico <- ggplot(datosGrafico) + 
-  geom_col(mapping = aes(x = Materia, y = nDOs)) + 
-  theme(axis.text.x = element_text(angle = 90))
 
+grafico <- ggplot(data = datosGrafico) +
+  geom_col(mapping = aes(x = Materia, fill = name, y = value),
+           position = position_stack(),
+           width = 0.75) +
+  geom_label(mapping = aes(x = Materia, 
+                           group = name, 
+                           y = value, 
+                           label = ifelse(value !=0, value, NA)),
+             position = position_stack(vjust = 0.5),
+             size = 2.5) +
+  scale_fill_brewer(palette = "Set1") +
+  scale_x_discrete(labels = label_wrap(28)) +
+  theme(axis.text.x = element_text(angle = 90),
+        panel.grid = element_line(colour = "black"),
+        panel.background = element_rect(fill = "white"),
+        legend.title = element_blank()) +
+  ggtitle(label = "Número de descriptores operativos por materia y competencia clave",
+          subtitle = "ESO") +
+  xlab(label = "") +
+  ylab(label = "")
 
-
+grafico
 
 # Peso de cada CC en cada materia
 # Análisis de componentes principales
