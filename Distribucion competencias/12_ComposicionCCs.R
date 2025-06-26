feca0d1ca4eb5f6d52d1e0813eb46c8fa88c6be0
Álvaro <alvaropereznarciandi@gmail.com>
2025-06-26 22:13:09 +0200

    Añado gráficos treemap, que permiten ver competencia a competencia qué materias tienen más peso

--- Distribucion competencias/12_ComposicionCCs.R
+++ Distribucion competencias/12_ComposicionCCs.R
@@ -1,10 +1,15 @@
 
+# Vacío la memoria de variables y paquetes - https://stackoverflow.com/a/41828719
+rm(list = ls())
+pacman::p_unload(pacman::p_loaded(), character.only = TRUE) 
+
 library(openxlsx)
 library(dplyr)
 library(ggplot2)
 library(tidyr)
 library(stringr)
-library(scales) # Para usar label_wrap
+library(pals)
+library(treemapify)
 
 #### CARGA DE DATOS ####
 
@@ -65,12 +70,18 @@ names(datosGrafico)[1] <- "CC"
 datosGrafico <- pivot_longer(data = datosGrafico,
                              cols = materias)
 
+datosGrafico$CC <- str_replace(string = datosGrafico$CC,
+                               pattern = "n",
+                               replacement = "")
+
+
+### GRÁFICO DE BARRAS ####
+
 # Sacada de un comentario en https://stackoverflow.com/a/9568659 
 paleta <- c("dodgerblue2", "#E31A1C", "green4", "#6A3D9A", "#FF7F00", 
             "black", "gold1", "skyblue2", "palegreen2", "#FDBF6F", "gray70", 
             "maroon", "orchid1", "darkturquoise", "darkorange4", "brown") 
-
-paleta <- pals::cols25(n = 24)
+paleta <- cols25(n = 24)
 
 grafico <- ggplot(data = datosGrafico) +
   geom_col(mapping = aes(x = CC, 
@@ -82,4 +93,65 @@ grafico <- ggplot(data = datosGrafico) +
   theme(legend.position = "bottom")
 
 grafico
+
+
+### TREEMAP ####
+
+# paleta <- brewer.blues(n = 24)
+# paleta <- linearl(n = 24)
+# paleta <- linearlhot(n = 24)
+# paleta <- kovesi.rainbow(n = 24)
+# paleta <- cols25(n = 24)
+
+paleta <- c("#00876c", "#3d9a70", "#64ad73", "#89bf77", "#afd17c",
+            "#d6e184", "#fff18f", "#fdd576", "#fbb862", "#f59b56",
+            "#ee7d4f", "#e35e4e", "#de425b", "#e9597c", "#f2719b",
+            "#f789b9", "#faa0d3", "#fcb8eb", "#ffceff", "#ecb2f4",
+            "#d696eb", "#bd7ce3", "#a263db", "#814bd5", "#5736cf")
+
+
+competencias <- c("CCL", "CP", "STEM", "CD", "CPSAA", "CC", "CE", "CCEC")
+
+nombresCompetencias <- c("Competencia en Comunicación Lingüística (CCL)", 
+                         "Competencia Plurilingue (CP)", 
+                         "Competencia en Ciencia, Tecnología, Ingeniería y Matemáticas (STEM)", 
+                         "Competencia Digital (CD)", 
+                         "Competencia Persona, Social y de Aprender a Aprender (CPSAA)", 
+                         "Competencia Ciudadana (CC)", 
+                         "Competencia Emprendedora (CE)", 
+                         "Competencia en Conciencia y Expresiones Culturales (CCEC)")
+
+
+for (competencia in competencias){
+  
+  rutaSalida <- paste0("./salida/PesoMaterias_",competencia,".png")
+  nombreCompetencia <- nombresCompetencias[competencia == competencias]
+  
+  grafico <- ggplot(data = datosGrafico %>% filter(CC == competencia)) +
+    geom_treemap(mapping = aes(area = value, 
+                               fill = name),
+                 layout = "srow",
+                 start = "topleft",
+                 color = "black") +
+    geom_treemap_text(mapping = aes(label = paste0(name,":\n",round(100*value),"%"),
+                                    area = value),
+                      place = "centre",
+                      reflow = TRUE,
+                      grow = FALSE,
+                      layout = "srow",
+                      start = "topleft") +
+    scale_fill_manual(values = paleta) +
+    theme(legend.position = "none",
+          plot.title = element_text(size = 25),
+          plot.subtitle = element_text(size = 20)) +
+    ggtitle(label = "Peso de cada materia en la competencia clave",
+            subtitle = nombreCompetencia)
+
+  ggsave(filename = rutaSalida,
+         plot = grafico,
+         units = "mm",
+         width = 200,
+         height = 200)
+}
+
 
